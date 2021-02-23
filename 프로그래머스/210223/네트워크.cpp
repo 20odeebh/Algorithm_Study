81acc2525ce451dd3ecae77ff985d88aab4c0604
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-02-23 12:12:22 +0900

    ADD:210223

---
+++ 프로그래머스/210223/네트워크.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+//연결 요소의 갯수 찾기
+bool c[200];
+vector<int> v[200];
+void dfs(int x) {
+    c[x] = true;
+    for (auto k : v[x]) {
+        if (c[k] == false)dfs(k);
+    }
+}
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    for (int i = 0; i < n; i++)c[i] = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j)continue;
+            if (computers[i][j])v[i].push_back(j);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (c[i] == false) {
+            dfs(i);
+            answer++;
+        }
+    }
+    return answer;
+}
---
+++ 프로그래머스/210223/정수 삼각형.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int d[501][501];
+//d[i][j] = i행 j 열에서 가질 수 있는 숫자 합의 최대값
+//d[i][j] = max(d[i-1][j],d[i-1][j-1])+a[i][j]
+//구현은 위층에서 아래층으로 최대값을 주는 방식
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int size = triangle.size();
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j <= i; j++) {
+            d[i][j] = triangle[i][j];
+        }
+    }
+    int size2 = size - 1;
+    for (int i = 0; i < size2; i++) {
+        for (int j = 0; j <= i; j++) {
+            d[i + 1][j] = max(d[i + 1][j], d[i][j] + triangle[i + 1][j]);
+            d[i + 1][j + 1] = max(d[i + 1][j + 1], d[i][j] + triangle[i + 1][j + 1]);
+        }
+    }
+    for (int i = 0; i < size; i++) {
+        answer = max(answer, d[size2][i]);
+    }
+    return answer;
+}
