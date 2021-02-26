5cb206dd021d6bdf701c0065ef7f2e3ef85a4df9
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-02-26 12:56:37 +0900

    ADD:210226

---
+++ 프로그래머스/210226/가장 긴 팰린드롬.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+bool d[2501][2501];
+//다이나믹 프로그래밍
+//d[i][j] = i~j 문자열이 팰린드롬이면 true
+//d[i][j] 는 s[i] == s[j] 이고 d[i+1][j-1]이 ture일때 팰린드롬이 된다.
+int solution(string s)
+{
+    int answer = 1;
+    int n = s.size();
+    for (int i = 0; i < n; i++)d[i][i] = true;
+    int n2 = n - 1;
+    for (int i = 0; i < n2; i++) {
+        if (s[i] == s[i + 1]) {
+            d[i][i + 1] = true;
+            answer = 2;
+        }
+    }
+    for (int i = 2; i < n; i++) {
+        for (int j = 0; j < s.size(); j++) {
+            int l = j, r = j + i;
+            if (r >= s.size())break;
+            if (s[l] == s[r]) {
+                if (d[l + 1][r - 1]) {
+                    d[l][r] = true;
+                    answer = i + 1;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
