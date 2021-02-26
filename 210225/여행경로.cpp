e0c54bf56dec875bdfadaf274c0003e4f4cbb6a8
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-02-26 18:58:55 +0900

    MOD : 210226

--- 210225/여행경로.cpp
+++ 210225/여행경로.cpp
@@ -1,44 +1,30 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int n;
-bool dfs(vector<vector<string>> const tickets, vector<string> &route, vector<bool> &visited, string dst){
-    route.push_back(dst);
-    
-	if(route.size() == n+1) return true;
-    
-	for(int i = 0; i<n; i++){
-        if(!visited[i] && tickets[i][0] == dst){
-            visited[i] = true;
-            if(dfs(tickets, route, visited, tickets[i][1])){
-                return true;
-            }
-            visited[i] = false;
-        }
+string ans = "a";
+int v[10000];
+void go(vector<vector<string>> tickets,string cur,string path,int len){
+    if(len == tickets.size()){
+        if(path<ans) ans=path;
+        return;
     }
+        for(int i=0;i<tickets.size();i++){
+            if(v[i] == 0 && tickets[i][0] == cur){
+                v[i]=1;
+                go(tickets,tickets[i][1],path+tickets[i][1],len+1);
+                v[i]=0;
+            }
+        }
     
-	route.pop_back();
-    return false;
+    
 }
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
-    n = tickets.size();
-    sort(tickets.begin(), tickets.end());
-    
-	for(int i = 0; i < n; i++){
-        vector<bool> visited(n, false);
-        vector<string> route;
-        
-		if(tickets[i][0] == "ICN"){
-            visited[i] = true;
-            route.push_back("ICN");
-            if(dfs(tickets, route, visited, tickets[i][1])){
-                answer = route;
-                break;
-            }
-           
-        }
+    go(tickets,"ICN","ICN",0);
+    for(int i=0;i<ans.size();i+=3){
+        answer.push_back(ans.substr(i,3));
     }
     return answer;
 }
