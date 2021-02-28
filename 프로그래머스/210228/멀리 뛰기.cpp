3b41db89cdbacc2236738ea709dac4b48e5e529a
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-02-28 12:00:50 +0900

    ADD:210228

---
+++ 프로그래머스/210228/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+//다이나믹 프로그래밍
+//d[i] = i번째 칸까지 올 수 있는 경우의 수
+const long long MOD = 1234567;
+long long d[2001];
+long long solution(int n) {
+    d[1] = 1;
+    d[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        d[i] = d[i - 1] + d[i - 2];
+        d[i] %= MOD;
+    }
+    return d[n];
+}
