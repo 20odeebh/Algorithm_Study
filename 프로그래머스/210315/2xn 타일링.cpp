7b8d54eee46ca2a453a04662d186514ce2c06f47
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-15 22:34:44 +0900

    ADD:210315

---
+++ 프로그래머스/210315/2xn 타일링.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int d[1001];
+const int mod = 10007;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	d[1] = 1;
+	d[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+		d[i] %= mod;
+	}
+	cout << d[n];
+}
---
+++ 프로그래머스/210315/괄호 추가하기.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+long long ans = INT64_MIN;
+//우선순위가 다른줄 알고 엄청 고민했던 문제
+//앞에꺼 더하고 넘기는지, 뒤에꺼 더한후 앞에꺼 더하고 넘기는지
+//완전탐색
+
+long long op(char oper,long long a,long long b) {
+	if (oper == '+')return a += b;
+	if (oper == '-')return a -= b;
+	if (oper == '*')return a *= b;
+}
+void go(int idx,long long sum) {
+	if (idx >= s.size()) {
+		ans = max(ans, sum);
+		return;
+	}
+	go(idx+2,op(s[idx], sum, s[idx + 1] - '0'));
+	if (idx + 2 < s.size()) {
+		long long temp=op(s[idx + 2], s[idx + 1] - '0', s[idx + 3] - '0');
+		go(idx + 4, op(s[idx], sum, temp));
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	cin >> s;
+	if (n == 1) {
+		cout << s[0] - '0';
+		return 0;
+	}
+	go(1, s[0] - '0');
+	cout << ans << '\n';
+}
