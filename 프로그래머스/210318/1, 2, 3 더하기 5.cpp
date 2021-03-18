75c5bae9805a4dd34614d4c72f9c8ac0e42d1280
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-18 18:44:23 +0900

    ADD:210318

---
+++ 프로그래머스/210318/1, 2, 3 더하기 5.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+long long d[100001][4];
+const long long MOD = 1000000009;
+//d[i][j]=  i라는 숫자를 더해서 만들때 끝자리가 j인 경우의 수
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	d[1][1] = d[2][2] = d[3][1] = d[3][2] = d[3][3] = 1;
+	for (int i = 4; i < 100001; i++) {
+		for (int j = 1; j <= 3; j++) {
+			for (int k = 1; k <= 3; k++) {
+				if (j == k)continue;
+				d[i][j] += d[i - j][k];
+				d[i][j] %= MOD;
+			}
+		}
+	}
+	int T;
+	cin >> T;
+	while (T--) {
+		int n;
+		cin >> n;
+		cout << (d[n][1] + d[n][2] + d[n][3]) % MOD << '\n';
+	}
+}
