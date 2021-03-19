fb634fab3dc438f6c4c37abeae676d9291c5e4a7
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-19 23:31:25 +0900

    ADD:210319

---
+++ 프로그래머스/210319/⚾.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int per[9],a[51][9],ru[4];
+//해시+완전탐색
+// 1~ 3루 주자까지 , 4루주자는 따로, 5~9루주자 해쉬로 돌리면서 탐색
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> a[i][j];
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < 8; i++) {
+		per[i] = i + 1;
+	}
+	do {
+		int ining = 0,tans=0,out_count=0,run_number=0;
+		while (true) {
+			if (a[ining][per[run_number]] == 0) {
+				out_count++;
+				if (out_count == 3) {
+					out_count = 0;
+					for (int i = 1; i < 4; i++)ru[i] = 0;
+					ining++;
+					if (ining == n)break;
+				}
+			}
+			else {
+				ru[0] = 1;
+				for (int i = 3; i >= 0; i--) {
+					if (ru[i]) {
+						if (i + a[ining][per[run_number]] >= 4) {
+							ru[i] = 0;
+							tans++;
+						}
+						else {
+							ru[i + a[ining][per[run_number]]] = 1;
+							ru[i] = 0;
+						}
+					}
+				}
+			}
+			run_number++;
+			if (run_number == 3)run_number = 8;
+			else if (run_number == 9)run_number = 3;
+			else if (run_number == 8)run_number = 0;
+		}
+		ans = max(ans, tans);
+	} while (next_permutation(per, per + 8));
+	cout << ans << '\n';
+}
---
+++ 프로그래머스/210319/쉬운 계단 수.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+//d[i][j]= 끝자리가 j인 i자리의 숫자
+long long d[101][10];
+const long long MOD = 1000000000;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	for (int i = 1; i < 10; i++) d[1][i] = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j > 0) {
+				d[i][j] += d[i - 1][j - 1];
+				d[i][j] %= MOD;
+			}
+			if (j < 9) {
+				d[i][j] += d[i - 1][j + 1];
+				d[i][j] %= MOD;
+			}
+		}
+	}
+	long long ans = 0;
+	for (int i = 0; i < 10; i++) {
+		ans += d[n][i];
+		ans %= MOD;
+	}
+	cout << ans;
+}
