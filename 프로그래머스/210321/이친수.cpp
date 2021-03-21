218d1ef857583b5a28642aad0738b40dface7307
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-21 22:08:15 +0900

    ADD:210321

---
+++ 프로그래머스/210321/이친수.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	long long d[100];
+	d[1] = 1;
+	d[2] = 1;
+	cin >> n;
+	for (int i = 3; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+	}
+	cout << d[n];
+
+}
