aaeddbcec02a1be4d8a383730eee0c60290362a1
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-25 00:45:41 +0900

    ADD:210325

---
+++ 프로그래머스/210325/빗물.cpp
@@ -0,0 +1,54 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int a[501][501];
+// 배열 그대로 만들어서 왼쪽 끝과 오른쪽 끝에 벽이 있는지 확인
+// 둘다 있으면 물을 2로 담아주고 최종적으로 2의 갯수를 센다.
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int t;
+        cin >> t;
+        int j2 = n - t;
+        for (int j = n - 1; j >= j2; j--) {
+            a[j][i] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        bool leftWall = false;
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == 0) {
+                int j2 = j + 1;
+                bool rightWall = false;
+                while (j2 < m) {
+                    if (a[i][j2] == 1) {
+                        rightWall = true;
+                        break;
+                    }
+                    else {
+                        j2++;
+                    }
+                }
+                if (leftWall && rightWall) {
+                    for (int k = j; k < j2; k++) {
+                        a[i][k] = 2;
+                    }
+                }
+                j = j2 - 1;
+            }
+            else {
+                leftWall = true;
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == 2) {
+                ans++;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
---
+++ 프로그래머스/210325/월드컵.cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int a[6][3];
+pair<int, int> p[15];
+// x와 y의 대결에서 승 0,무, 패 에 대한 모든 경우의수를
+// dfs + 백트래킹으로 완전탐색한다.
+bool ansFlag = false;
+void go(int idx) {
+    if (ansFlag)return;
+    if (idx == 15) {
+        bool temp = true;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (a[i][j]) {
+                    temp = false;
+                    break;
+                }
+            }
+            if (temp == false)break;
+        }
+        if (temp)ansFlag = true;
+        return;
+    }
+    int x = p[idx].first, y = p[idx].second;
+    for (int i = 0; i < 3; i++) {
+        if (a[x][i] && a[y][2 - i]) {
+            a[x][i]--;
+            a[y][2 - i]--;
+            go(idx + 1);
+            a[x][i]++;
+            a[y][2 - i]++;
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int T = 4;
+    int idx = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 6; j++) {
+            p[idx].first = i;
+            p[idx++].second = j;
+        }
+    }
+    while (T--) {
+        int ans = 1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                cin >> a[i][j];
+            }
+        }
+        ansFlag = false;
+        go(0);
+        if (ansFlag)cout << "1 ";
+        else cout << "0 ";
+
+    }
+}
