207717c9fbeb58001d6836c1c921ae3202b8e6f2
upsk1 <55921031+upsk1@users.noreply.github.com>
2021-03-28 21:55:54 +0900

    ADD:210328

---
+++ 프로그래머스/210328/문자열 폭발.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+string a, b;
+stack<char> st;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> a >> b;
+	char blast = b[b.size() - 1];
+	for (int i = 0; i < a.size(); i++) {
+		st.push(a[i]);
+		//스택에 있는 글자가 B의 마지막 글자면
+		if (a[i] == blast) {
+			if (st.size() >= b.size()) {
+				string temp = "";
+				for (int j = 0; j < b.size(); j++) {
+					temp += st.top();
+					st.pop();
+				}
+				bool flag = true;
+				//하나라도 틀리면 다시 스택에 푸쉬
+				for (int j = 0; j < b.size(); j++) {
+					if (b[j] != temp[b.size() - j - 1]) {
+						flag = false;
+						break;
+					}
+				}
+				if (flag == false) {
+					for (int j = b.size() - 1; j >= 0; j--) {
+						st.push(temp[j]);
+					}
+				}
+			}
+		}
+	}
+	string ans = "";
+	if (st.empty()) {
+		cout << "FRULA";
+		return 0;
+	}
+	while (!st.empty()) {
+		ans += st.top();
+		st.pop();
+	}
+	for (int i = ans.size() - 1; i >= 0; i--) {
+		cout << ans[i];
+	}
+}
---
+++ 프로그래머스/210328/접두사.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+string s[51];
+stack<string> st;
+// 정렬하면 접두어가 가장 앞에 있게된다. 접두어들을 빼면서 스택에 넣고
+// 최종적으로 스택의 사이즈가 정답이 된다.
+// 증명은 어려우나 왠지 될 것 같았음.
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)cin >> s[i];
+	sort(s, s + n);
+	st.push(s[0]);
+	for (int i = 1; i < n; i++) {
+		string s2 = st.top();
+		if (s2.size() <= s[i].size()) {
+			bool flag = true;
+			for (int j = 0; j < s2.size(); j++) {
+				if (s2[j] != s[i][j]) {
+					flag = false;
+					break;
+				}
+			}
+			if(flag)st.pop();
+		}
+		st.push(s[i]);
+	}
+	cout << st.size() << '\n';
+}
